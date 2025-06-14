6f5cd35bd8e0bbc0bd09af9a22a2c80641db8806
Marcus <marcus.mendonca@gafit.com.br>
2025-06-14 16:49:17 -0300

    Chore: create Photo model and migration

---
+++ app/Models/Photo.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Photo extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'student_id',
+        'path',
+        'filesize',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'filesize' => 'integer',
+        ];
+    }
+
+    /**
+     * Get the student that owns the photo.
+     */
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class);
+    }
+}
