ae4b4f51dc735d7bdab204791e128d0da71277ed
Marcus <marcus.mendonca@gafit.com.br>
2025-06-14 17:35:48 -0300

    Chore: AdminMiddleware

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Enums\UserType;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->user() && $request->user()->type === UserType::ADMIN) {
+            return $next($request);
+        }
+
+        abort(403, 'Acesso não autorizado.');
+    }
+}
