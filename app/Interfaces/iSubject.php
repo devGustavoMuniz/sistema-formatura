961dd88afc85fc7a18cc426dd1a2d28e38569a74
Marcus <marcus.mendonca@gafit.com.br>
2025-06-14 17:13:02 -0300

    Chore: Subject Interface

---
+++ app/Interfaces/iSubject.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Interfaces;
+
+/**
+ * Interface para os objetos Observados (Subjects).
+ * Define os métodos para gerenciar os Observers.
+ */
+interface iSubject
+{
+    /**
+     * Anexa um Observer ao Subject.
+     *
+     * @param iObserver $observer O Observer a ser anexado.
+     * @return void
+     */
+    public function addObserver(iObserver $observer): void;
+
+    /**
+     * Desanexa um Observer do Subject.
+     *
+     * @param iObserver $observer O Observer a ser desanexado.
+     * @return void
+     */
+    public function removeObserver(iObserver $observer): void;
+
+    /**
+     * Notifica todos os Observers anexados sobre uma mudança.
+     *
+     * @return void
+     */
+    public function notify(): void;
+}
